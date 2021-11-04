0ddef02c1e46e2141ef3521c84a2fb6948b7c21f
Mark Pokrovsky <solexofficial@yandex.ru>
2021-11-04 15:36:06 +0300

    feat: add header

---
+++ src/app/components/common/header.jsx
@@ -0,0 +1,38 @@
+import { AppBar, Button, Toolbar, Typography, Container } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import HomeLogo from '../icons/logo';
+
+const Header = () => {
+  return (
+    <Box sx={{ flexGrow: 1, zIndex: 1000 }}>
+      <Container>
+        <AppBar position='static' color='inherit'>
+          <Toolbar>
+            <HomeLogo viewBox='0 0 40 40' />
+            <Typography
+              variant='h6'
+              component='h1'
+              sx={{ textTransform: 'uppercase', ml: '13px', fontWeight: 400, flexGrow: 1 }}
+            >
+              Toxin
+            </Typography>
+            <NavLink to='/login' style={{ textDecoration: 'none' }}>
+              <Button variant='outlined' size='small' sx={{ mr: 2 }}>
+                Войти
+              </Button>
+            </NavLink>
+            <NavLink to='/login' style={{ textDecoration: 'none' }}>
+              <Button variant='contained' size='small'>
+                Зарегистрироваться
+              </Button>
+            </NavLink>
+          </Toolbar>
+        </AppBar>
+      </Container>
+    </Box>
+  );
+};
+
+export default Header;
