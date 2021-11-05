c411009edfe7fd382fde6837cb589002d2848f04
Mark Pokrovsky <solexofficial@yandex.ru>
2021-11-05 15:50:29 +0300

    feat: add change data datePickerField

--- src/app/components/common/form/datePickerField.jsx
+++ src/app/components/common/form/datePickerField.jsx
@@ -3,11 +3,10 @@ import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import ruLocale from 'date-fns/locale/ru';
 import DatePicker from '@mui/lab/DatePicker';
-import React, { useState } from 'react';
+import React from 'react';
 import { Box } from '@mui/system';
 
-const DatePickerField = ({ label, ...rest }) => {
-  const [value, setValue] = useState(null);
+const DatePickerField = ({ label, name, value, setData, ...rest }) => {
   return (
     <Box style={{ margin: '15px 0' }}>
       <LocalizationProvider dateAdapter={AdapterDateFns} locale={ruLocale}>
@@ -16,7 +15,10 @@ const DatePickerField = ({ label, ...rest }) => {
           label={label}
           value={value}
           onChange={newValue => {
-            setValue(newValue);
+            setData(prevState => ({
+              ...prevState,
+              [name]: newValue,
+            }));
           }}
           {...rest}
           renderInput={params => <TextField {...params} size='small' />}
--- src/app/components/ui/searchRoomsForm.jsx
+++ src/app/components/ui/searchRoomsForm.jsx
@@ -2,13 +2,12 @@ import { ArrowRight } from '@mui/icons-material';
 import { Button, Card, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import declOfNum from '../../utils/declOfNum';
-import FormComponent from '../common/form';
 import DatePickerField from '../common/form/datePickerField';
 import NumberField from '../common/form/numberField';
 import Title from '../common/typography/title';
 import MyAccordion from './accordion';
 
-const initialState = { adults: 0, children: 0, babies: 0 };
+const initialState = { adults: 0, children: 0, babies: 0, arrival: null, departure: null };
 const SearchRoomsForm = () => {
   const [data, setData] = useState(initialState);
 
@@ -17,19 +16,9 @@ const SearchRoomsForm = () => {
     console.log(data);
   };
 
-  // const handleChange = useCallback(target => {
-  //   console.log(target.name);
-  //   setData(prevState => ({
-  //     ...prevState,
-  //     [target.name]: target.value,
-  //   }));
-  // }, []);
-
   const handleReset = () => {
     setData(initialState);
   };
-
-  const validatorConfig = {};
 
   const getAccordionLabel = () => {
     const countGuests = data.adults + data.children + data.babies;
@@ -48,34 +37,46 @@ const SearchRoomsForm = () => {
 
   return (
     <>
-      <FormComponent onSubmit={handleSubmit} validatorConfig={validatorConfig} defaultData={data}></FormComponent>
-      <Card raised sx={{ mt: '70px', padding: '30px', paddingTop: '40px', background: '#fff', width: 380 }}>
+      <Card
+        raised
+        sx={{ mt: '70px', padding: '30px', paddingTop: '40px', background: '#fff', width: 380 }}
+        component='form'
+      >
         <Title isBold>Найдём номера под ваши пожелания</Title>
-        {/* <FormComponent onSubmit={handleSubmit} validatorConfig={validatorConfig} defaultData={data}></FormComponent> */}
-        <form>
-          <DatePickerField label='Дата прибытия' style={{ margin: '15px 0' }} />
-          <DatePickerField label='Дата выезда' style={{ margin: '15px 0' }} />
-          <Typography sx={{ marginTop: '20px', fontWeight: 700 }}>Гости</Typography>
-          <MyAccordion label={getAccordionLabel()}>
-            <NumberField label='Взрослые' name='adults' data={data} setData={setData} />
-            <NumberField label='Дети' name='children' data={data} setData={setData} />
-            <NumberField label='Младенцы' name='babies' data={data} setData={setData} />
-          </MyAccordion>
-          <Button variant='outlined' size='small' onClick={handleReset} sx={{ mt: '15px' }}>
-            Очистить
-          </Button>
-          <Button
-            variant='contained'
-            size='large'
-            color='primary'
-            endIcon={<ArrowRight />}
-            sx={{ mt: '30px' }}
-            onClick={handleSubmit}
-            fullWidth
-          >
-            Подобрать номер
-          </Button>
-        </form>
+        <DatePickerField
+          label='Дата прибытия'
+          style={{ margin: '15px 0' }}
+          value={data.arrival}
+          setData={setData}
+          name='arrival'
+        />
+        <DatePickerField
+          label='Дата выезда'
+          style={{ margin: '15px 0' }}
+          value={data.departure}
+          setData={setData}
+          name='departure'
+        />
+        <Typography sx={{ marginTop: '20px', fontWeight: 700 }}>Гости</Typography>
+        <MyAccordion label={getAccordionLabel()}>
+          <NumberField label='Взрослые' name='adults' data={data} setData={setData} />
+          <NumberField label='Дети' name='children' data={data} setData={setData} />
+          <NumberField label='Младенцы' name='babies' data={data} setData={setData} />
+        </MyAccordion>
+        <Button variant='outlined' size='small' onClick={handleReset} sx={{ mt: '15px' }}>
+          Очистить
+        </Button>
+        <Button
+          variant='contained'
+          size='large'
+          color='primary'
+          endIcon={<ArrowRight />}
+          sx={{ mt: '30px' }}
+          onClick={handleSubmit}
+          fullWidth
+        >
+          Подобрать номер
+        </Button>
       </Card>
     </>
   );
