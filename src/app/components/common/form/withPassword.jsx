e4ef2ed558fc3ae2d809f58e91d82b987ce699b5
Mark Pokrovsky <solexofficial@yandex.ru>
2021-11-06 15:43:04 +0300

    feat: add show password btn HOC withPassword textField

--- src/app/components/common/form/withPassword.jsx
+++ src/app/components/common/form/withPassword.jsx
@@ -1,22 +1,39 @@
-import { Box } from '@mui/system';
-import React, { useCallback, useState } from 'react';
+import { FormControl, IconButton, InputAdornment } from '@material-ui/core';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import React, { useState } from 'react';
 
 const withPassword = Component => props => {
   const [showPassword, setShowPassword] = useState(false);
 
-  const toggleShowPassword = useCallback(() => {
+  const toggleShowPassword = () => {
     setShowPassword(prevState => !prevState);
-  });
+  };
+
+  const handleMouseDownPassword = e => {
+    e.preventDefault();
+  };
 
   return (
-    <Box>
-      <Component {...props} type={showPassword ? 'text' : 'password'} />
-      {
-        <button type='button' onClick={toggleShowPassword}>
-          SHOW PASS
-        </button>
-      }
-    </Box>
+    <FormControl>
+      <Component
+        {...props}
+        type={showPassword ? 'text' : 'password'}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position='end'>
+              <IconButton
+                aria-label='toggle password visibility'
+                onClick={toggleShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge='end'
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </FormControl>
   );
 };
 
