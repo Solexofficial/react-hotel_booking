29325ea21436d48dc37a6b2a38f65f296dd32f80
Mark Pokrovsky <solexofficial@yandex.ru>
2021-11-07 23:27:53 +0300

    feat: add container

---
+++ src/app/components/common/container.jsx
@@ -0,0 +1,11 @@
+import { Container as MuiContainer } from '@mui/material';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Container = ({ children }) => {
+  return <MuiContainer>{children}</MuiContainer>;
+};
+Container.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+};
+export default Container;
