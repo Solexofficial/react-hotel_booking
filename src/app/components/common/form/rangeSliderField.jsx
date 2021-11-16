9b5f2c13fdd748c9017ad486495de4ca5dc3d7e0
Mark Pokrovsky <solexofficial@yandex.ru>
2021-11-16 22:40:28 +0300

    refactor: rechange Box mui to div

--- src/app/components/common/form/rangeSliderField.jsx
+++ src/app/components/common/form/rangeSliderField.jsx
@@ -1,5 +1,4 @@
 import { Slider } from '@mui/material';
-import { Box } from '@mui/system';
 import React, { useCallback, useEffect, useState } from 'react';
 import Text from '../typography/text';
 import Title from '../typography/title';
@@ -38,15 +37,15 @@ const RangeSliderField = ({ label, name, onChange, value, min, max, minDistance 
   console.log('render range slider');
   if (value) {
     return (
-      <Box>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px 0' }}>
-          <Title component='h3' variant='subtitle2' isBold>
+      <div>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px 0' }}>
+          <Title component='span' variant='subtitle2' isBold>
             {label || 'Range Slider'}
           </Title>
           <Text component='span' variant='subtitle2' sx={{ fontSize: '12px' }}>
             {value[0]}&#8381; - {value[1]}&#8381;
           </Text>
-        </Box>
+        </div>
         <Slider
           name={name}
           value={sliderValue}
@@ -62,7 +61,7 @@ const RangeSliderField = ({ label, name, onChange, value, min, max, minDistance 
         <Text variant='subtitle2' component='p' sx={{ fontSize: '12px' }}>
           Стоимость за сутки пребывания в номере
         </Text>
-      </Box>
+      </div>
     );
   }
   return <p>Loading...</p>;
