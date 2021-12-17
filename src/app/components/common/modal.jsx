ca7232f60b2eaeddc8c0dfbedce2e890955f22e3
Mark Pokrovsky <solexofficial@yandex.ru>
2021-12-17 23:12:04 +0300

    feat: modal: add backdrop if async cb fetching data

--- src/app/components/common/modal.jsx
+++ src/app/components/common/modal.jsx
@@ -3,18 +3,23 @@ import { IconButton } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import React from 'react';
+import Backdrop from './backdrop';
 
-const Modal = ({ open, onClose, title, children }) => {
+const Modal = ({ open, onClose, title, isLoading = false, children }) => {
+  console.log(isLoading);
   return (
-    <Dialog className='modal' onClose={onClose} open={open} keepMounted>
-      <DialogTitle className='modal-title'>
-        {title}
-        <IconButton className='modal-btn__close' onClick={onClose}>
-          <CloseIcon aria-label='close' />
-        </IconButton>
-      </DialogTitle>
-      {children}
-    </Dialog>
+    <>
+      <Dialog className='modal' onClose={onClose} open={open} keepMounted>
+        <DialogTitle className='modal-title'>
+          {title}
+          <IconButton className='modal-btn__close' onClick={onClose}>
+            <CloseIcon aria-label='close' />
+          </IconButton>
+        </DialogTitle>
+        {children}
+      </Dialog>
+      {isLoading && <Backdrop isOpen={isLoading} />}
+    </>
   );
 };
 
