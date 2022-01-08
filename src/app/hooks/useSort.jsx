d9daa112871c6672a25f2b8db051a635336a92c7
Mark Pokrovsky <solexofficial@yandex.ru>
2022-01-08 15:11:02 +0300

    refactor: useSort

--- src/app/hooks/useSort.jsx
+++ src/app/hooks/useSort.jsx
@@ -1,9 +1,35 @@
-import orderBy from 'lodash/orderBy';
-
 const useSort = (items, sortBy) => {
-  const sortedItems = orderBy(items, [sortBy.path], [sortBy.order]);
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
 
-  return { sortedItems };
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
+  function stableSort(array = [], comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) {
+        return order;
+      }
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map(el => el[0]);
+  }
+
+  const sortedItems = stableSort(items, getComparator(sortBy.order, sortBy.path));
+
+  return sortedItems;
 };
 
 export default useSort;
