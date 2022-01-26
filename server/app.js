35898ae1f40a8cbf4fa50f6c94267e0fcb36ed0c
Mark Pokrovsky <solexofficial@yandex.ru>
2022-01-26 19:20:31 +0300

    feat: add connect to mongoDB

--- server/app.js
+++ server/app.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const chalk = require('chalk');
+const dotenv = require('dotenv');
+const initDatabase = require('./start/initDatabase');
 
-const express = express();
+const app = express();
+dotenv.config();
 
-express.use(express.json());
-express.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = config.get('port') ?? 8080;
+
+async function start() {
+  try {
+    mongoose.connection.once('open', () => {
+      initDatabase();
+    });
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(chalk.green('MongoDB connected.'));
+    app.listen(PORT, () => console.log(chalk.green(`Server has been started on port ${PORT}...`)));
+  } catch (error) {
+    console.log(chalk.red(error.message));
+    process.exit(1);
+  }
+}
+
+start();
---
+++ server/start/initDatabase.js
@@ -0,0 +1,3 @@
+module.exports = async () => {
+  console.log('init db');
+};
