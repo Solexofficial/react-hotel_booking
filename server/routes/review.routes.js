0745b12adace3958cb7d9685e7b54156153febbe
Mark Pokrovsky <solexofficial@yandex.ru>
2022-01-31 16:02:02 +0300

    fix: imports

--- server/routes/review.routes.js
+++ server/routes/review.routes.js
@@ -4,8 +4,43 @@ const router = express.Router({ mergeParams: true });
 
 router.get('/', async (req, res) => {
   try {
-    const reviews = await Review.find();
+    const { orderBy, equalTo } = req.query;
+    const reviews = await Review.find({ [orderBy]: equalTo });
     res.status(200).send(reviews);
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже',
+    });
+  }
+});
+
+router.post('/', auth, async (req, res) => {
+  try {
+    const newReview = await Review.create({
+      ...req.body,
+      userId: req.user_id,
+    });
+    res.status(201).send(newReview);
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже',
+    });
+  }
+});
+
+router.delete('/:reviewId', auth, (req, res) => {
+  try {
+    const { reviewId } = req.params;
+    const removedReview = await Review.findById(reviewId);
+
+    if (removedReview.userId.toString() === req.user._id) {
+      await removedReview.remove();
+      return res.send(null);
+    } else {
+      res.status(401).json({
+        message: 'Unauthorized',
+      });
+    }
   } catch (error) {
     res.status(500).json({
       message: 'На сервере произошла ошибка. Попробуйте позже',
