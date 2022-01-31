f77d1304ded40ffd4c96c3ee363369ecd9f75cb8
Mark Pokrovsky <solexofficial@yandex.ru>
2022-01-31 16:02:02 +0300

    feat: add post, delete booking, like, review entities

--- server/routes/booking.routes.js
+++ server/routes/booking.routes.js
@@ -4,8 +4,43 @@ const router = express.Router({ mergeParams: true });
 
 router.get('/', async (req, res) => {
   try {
-    const bookings = await Booking.find();
+    const { orderBy, equalTo } = req.query;
+    const bookings = await Booking.find({ [orderBy]: equalTo });
     res.status(200).send(bookings);
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже',
+    });
+  }
+});
+
+router.post('/', auth, async (req, res) => {
+  try {
+    const newBooking = await Booking.create({
+      ...req.body,
+      userId: req.user_id,
+    });
+    res.status(201).send(newBooking);
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже',
+    });
+  }
+});
+
+router.delete('/:bookingId', auth, (req, res) => {
+  try {
+    const { bookingId } = req.params;
+    const removedBooking = await Booking.findById(bookingId);
+
+    if (removedBooking.userId.toString() === req.user._id) {
+      await removedBooking.remove();
+      return res.send(null);
+    } else {
+      res.status(401).json({
+        message: 'Unauthorized',
+      });
+    }
   } catch (error) {
     res.status(500).json({
       message: 'На сервере произошла ошибка. Попробуйте позже',
--- server/routes/like.routes.js
+++ server/routes/like.routes.js
@@ -4,8 +4,43 @@ const router = express.Router({ mergeParams: true });
 
 router.get('/', async (req, res) => {
   try {
-    const likes = await Like.find();
+    const { orderBy, equalTo } = req.query;
+    const likes = await Like.find({ [orderBy]: equalTo });
     res.status(200).send(likes);
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже',
+    });
+  }
+});
+
+router.post('/', auth, async (req, res) => {
+  try {
+    const newLike = await Like.create({
+      ...req.body,
+      userId: req.user_id,
+    });
+    res.status(201).send(newLike);
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже',
+    });
+  }
+});
+
+router.delete('/:likeId', auth, (req, res) => {
+  try {
+    const { likeId } = req.params;
+    const removedLike = await Like.findById(likeId);
+
+    if (removedLike.userId.toString() === req.user._id) {
+      await removedLike.remove();
+      return res.send(null);
+    } else {
+      res.status(401).json({
+        message: 'Unauthorized',
+      });
+    }
   } catch (error) {
     res.status(500).json({
       message: 'На сервере произошла ошибка. Попробуйте позже',
