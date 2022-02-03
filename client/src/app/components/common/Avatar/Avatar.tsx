65725b51bf67b823863ab1ff2e07ff868526d298
Mark Pokrovsky <solexofficial@yandex.ru>
2022-02-03 23:29:10 +0300

    refactor: Avatar with types

--- client/src/app/components/common/Avatar/Avatar.tsx
+++ client/src/app/components/common/Avatar/Avatar.tsx
@@ -1,7 +1,13 @@
 import { Avatar as MuiAvatar } from '@mui/material';
 import React from 'react';
 
-const Avatar = ({ alt, src, className }) => {
+type AvatarProps = {
+  alt: string;
+  src: string;
+  className: string;
+};
+
+const Avatar: React.FC<AvatarProps> = ({ alt, src, className }) => {
   return <MuiAvatar alt={alt} src={src} className={className} />;
 };
 
