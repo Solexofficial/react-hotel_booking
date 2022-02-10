13f500e9828210d4553631525ecb0b9fac867107
Mark Pokrovsky <solexofficial@yandex.ru>
2022-02-11 02:27:04 +0300

    refactor: room page cards to TS

--- client/src/app/components/ui/RoomPageCards/RoomReviewsCard.tsx
+++ client/src/app/components/ui/RoomPageCards/RoomReviewsCard.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import declOfNum from '../../../utils/declOfNum';
 
-const RoomReviewsCard = ({ countReviews }) => {
+const RoomReviewsCard = () => {
   return (
     <div className='room-info__card'>
       <h3 className='room-info__card-title'>Впечатления от номера</h3>
       Скоро здесь будет статистика по отзывам! Приносим извинения за неудобства.
-      {/* <span className='room-info__card-rating'>{`${countReviews} ${declOfNum(countReviews, [
-        'Отзыв',
-        'Отзыва',
-        'Отзывов',
-      ])}`}</span> */}
     </div>
   );
 };
