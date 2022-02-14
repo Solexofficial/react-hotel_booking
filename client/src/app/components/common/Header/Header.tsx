ddcfbf41f11b70ceaf560a9107e82cffe0bb1508
Mark Pokrovsky <solexofficial@yandex.ru>
2022-02-14 23:50:28 +0300

    refactor: header add authErrors case

--- client/src/app/components/common/Header/Header.tsx
+++ client/src/app/components/common/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { navigationRoutes } from '../../../router/routes';
-import { getIsLoggedIn } from '../../../store/users';
+import { getAuthErrors, getIsLoggedIn } from '../../../store/users';
 import NavProfile from '../../ui/NavProfile';
 import Button from '../Button';
 import Container from '../Container';
@@ -12,6 +12,7 @@ import NavList from '../NavList';
 
 const Header: React.FC = () => {
   const isLoggedIn = useSelector(getIsLoggedIn());
+  const authErrors = useSelector(getAuthErrors());
 
   return (
     <header className='header'>
@@ -19,7 +20,7 @@ const Header: React.FC = () => {
         <div className='header__inner'>
           <Logo className='header__logo' />
           <NavList routes={navigationRoutes} className='header-nav' />
-          {isLoggedIn ? (
+          {isLoggedIn && !authErrors ? (
             <>
               <Divider orientation='vertical' flexItem className='header__divider' />
               <NavProfile />
